f2dbf958024417752aaf3c9281b84d791ed57c5f
Petra <144622676+knastman@users.noreply.github.com>
2024-06-17 17:53:09 +0200

    Edit fix & cleanup

--- app.js
+++ app.js
@@ -19,58 +19,107 @@ async function getTables(){
 }
 
 
-
 /******************
 Routing
 ******************/
 
-
-// STARTAR HÄR - välja tabell och visa innehållet
 app.get('/', async (req, res) => {
-  const pageTitle = "SQL - Databas GritAcademy";
+  let pageTitle = "Databas - GritAcademy";
   const dbTables = await getTables();
   let tableName = "select table";
-  res.render('index', {pageTitle, tableName, dbTables, tableName} );
+  res.render('index', {pageTitle, tableName, dbTables} );
 });
 
-
-let currentTable;
+//Hämtar info beroende på användarens val av tabell
 app.post('/', async (req, res) => {
-  console.log(req.body); 
   const dbTables = await getTables();
-
-  const pageTitle = "Select row"; 
+  let pageTitle = 'Databas - GritAcademy'; 
   let errorMsg = "";
   let tableName = "";
   let dbData=[];
 
-
-  if(req.body.tableMenuBtn){
+  if(req.body.tableMenuBtn){ 
     let tableName = req.body.tableMenuBtn;
-    let sql = `SELECT * FROM ${req.body.tableMenuBtn}`;
+    let sql = `SELECT * FROM ${tableName}`;
     dbData = await db.query(sql);
     res.render('index', {pageTitle,tableName, dbData, dbTables, errorMsg});
   } 
-  else if(req.body.delRowBtn){
+  else if(req.body.delRowBtn){ //Hämtar info efter raderad rad?
     let tableName = req.body.delRowBtn.split(",")[0];
-    let sql = `SELECT * FROM ${req.body.delRowBtn.split(",")[0]}`;  
+    let id = req.body.delRowBtn.split(",")[1];
+
+    //remove row
+    await db.query(`DELETE FROM ${tableName} WHERE id=${id}`);
+
+    let sql = `SELECT * FROM ${tableName}`;
     dbData = await db.query(sql);
     res.render('index', {pageTitle,tableName, dbData, dbTables, errorMsg});
+
   }
+
   else{
     errorMsg = "table name missing";
     dbData = [{}];
     res.render('index', {pageTitle,tableName, dbData, dbTables, errorMsg});
   }
 
-  //remove row
-  if(req.body.delRowBtn){
-    await db.query(`DELETE FROM ${req.body.delRowBtn.split(",")[0]} WHERE id=${req.body.delRowBtn.split(",")[1]}`);
-  }
-
 });
 
 
+/****************************
+ EDIT POST 
+*********************/
+
+//ID från URL
+app.get('/editData', async (req, res) => {
+  const {id, table} = req.query;
+  let successMsg ='Redigera nedan';
+
+  if (!id) {
+    return res.status(400).send('ID is required');
+  }
+
+  const sql = `SELECT * FROM ${table} WHERE id = ${id}; `;
+  // console.log('sql i get editdata get');
+  // console.log(sql);
+
+  try {
+    const dbData = await db.query(sql);
+    res.render('editData', { table, id, dbData, successMsg });
+
+  } catch (error) {
+    console.error('Error executing SQL query:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.post('/editData', async (req, res) => {
+  let id = req.body.editBtn.split(",")[1];
+  let table = req.body.editBtn.split(",")[0];
+
+  let successMsg ='Edit data';
+  let sql = '';
+  let dbData = null;
+    for (const [key, value] of Object.entries(req.body)) {
+      if (key !== 'editBtn' && key !== 'id'){
+        sql = `UPDATE ${table} SET ${key}="${value}" WHERE id=${id}`;
+        try {
+          dbData = await db.query(sql, id);
+      
+        } catch (error) {
+          console.error('Error executing SQL query:', error);
+          res.status(500).send('Internal Server Error');
+        }
+      }
+    }
+
+    sql = `SELECT * FROM ${table} WHERE id = ${id}; `;
+    dbData = await db.query(sql, id);
+    successMsg ='Posten har blivit redigerad';
+    res.render('editData', { successMsg, id, table, dbData });
+
+});
 
 
 
@@ -92,7 +141,6 @@ app.get('/students', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
 
 
 //Studentinfo ID från URL - what courses
@@ -132,6 +180,53 @@ app.get('/student', async (req, res) => {
     const studentId = ` ${id}`;
 
     res.render('student', { pageTitle, studentId, studentName, studentFname, studentLname, studentTown, studentInfo, dbData: coursesData });
+
+  } catch (error) {
+    console.error('Error executing SQL query:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+//info för specifik parameter
+app.get('/moreinfo', async (req, res) => {
+  const { id, town, fname, lname } = req.query;
+
+  let sql = '';
+  let queryParams = [];
+  let paramName = '';
+
+  if (id) {
+    sql = `SELECT * FROM students WHERE id = ${id}`;
+    queryParams.push(id);
+  } else if (town) {
+    sql = `SELECT * FROM students WHERE town = '${town}'`;
+    queryParams.push(town);
+    paramName = 'bor i ' + town;
+  } else if (fname) {
+    sql = `SELECT * FROM students WHERE fname = '${fname}'`;
+    queryParams.push(fname);
+    paramName = 'heter ' + fname + ' i förnamn';
+  } else if (lname) {
+    sql = `SELECT * FROM students WHERE lname = '${lname}'`;
+    queryParams.push(lname);
+    paramName = 'heter ' + lname + ' i efternamn';
+  } else {
+    return res.status(400).send('A valid query parameter (id, town, fname, lname) is required');
+  }
+
+  try {
+    const dbData = await db.query(sql, queryParams);
+
+    if (dbData.length === 0) {
+      return res.status(404).send('No matching records found');
+    }
+
+    const pageTitle = 'More Information on parameter';
+    console.log('dbData');
+    console.log(dbData);
+    res.render('moreinfo', { pageTitle, dbData, paramName });
 
   } catch (error) {
     console.error('Error executing SQL query:', error);
@@ -206,179 +301,13 @@ app.get('/course', async (req, res) => {
 
 
 
-/*****EDIT */
-
-//ID från URL
-app.get('/editData', async (req, res) => {
-  const {id, table} = req.query;
- 
-  if (!id) {
-    return res.status(400).send('ID is required');
-  }
-
-  const sql = `SELECT * FROM ${table} WHERE id = ${id}; `;
-
-
-  // const coursesSql= `SELECT courses.id, courses.name FROM courses
-  // JOIN students_courses ON courses.id = students_courses.courses_id
-  // JOIN students ON students_courses.students_id = students.id
-  // WHERE students.id = ${id}; `;
-
-  try {
-    const dbData = await db.query(sql, id);
-    const pageTitle = `Edit data`;
-    const tableName = `${table}`;
-    const rowId = `${id}`;
-    res.render('editData', { pageTitle, tableName, rowId, dbData });
-
-  } catch (error) {
-    console.error('Error executing SQL query:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-
-
-//EJ KLAR
-app.post('/editData', async (req, res) => {
-  console.log('req.body'); 
-  console.log(req.body); 
-  const {id, table} = req.query;
-  // const id = `${req.body.split(",")[1]};`
-
-  if (!id) {
-    return res.status(400).send('ID is required');
-  }
-
-  // console.log(id);
-  // const sql = `UPDATE ${table} SET ${value} = ${newValue}  WHERE id = ${id}; `;
-  // const sql = `UPDATE ${table} SET ${value} = ${newValue}  WHERE id = ${req.body.split(",")[1]}; `;
-  // const sql = `UPDATE ${table} SET ${value} = ${newValue}  WHERE id = ${rowID}; `;
-  
-  
-  try {
-    console.log('sql');
-    console.log(sql);
-
-    const dbData = await db.query(sql, id);
-    const pageTitle = `Edit data`;
-    const tableName = `${table}`;
-    // const id = `${id}`;
-
-    res.render('editData', { pageTitle, tableName, dbData });
-
-  } catch (error) {
-    console.error('Error executing SQL query:', error);
-    res.status(500).send('Internal Server Error');
-  }
-
-});
-
-
-
-
-
-// //edit and info page EJ KLAR
-// app.post('/editData', async (req, res) => {
-//   const { id, town, fname, lname } = req.query;
-
-//   let sql = '';
-//   let queryParams = [];
-
-//   if (id) {
-//     sql = `SELECT * FROM students WHERE id = ${id}`;
-//     queryParams.push(id);
-//   } else if (town) {
-//     sql = `SELECT * FROM students WHERE town = '${town}'`;
-//     queryParams.push(town);
-//   } else if (fname) {
-//     sql = `SELECT * FROM students WHERE fname = '${fname}'`;
-//     queryParams.push(fname);
-//   } else if (lname) {
-//     sql = `SELECT * FROM students WHERE lname = '${lname}'`;
-//     queryParams.push(lname);
-//   } else {
-//     return res.status(400).send('A valid query parameter (id, town, fname, lname) is required');
-//   }
-
-//   try {
-//     const dbData = await db.query(sql, queryParams);
-
-//     if (dbData.length === 0) {
-//       return res.status(404).send('No matching records found');
-//     }
-
-//     const pageTitle = 'More Information';
-//     console.log('dbData');
-//     console.log(dbData);
-//     res.render('moreinfo', { pageTitle, dbData });
-
-//   } catch (error) {
-//     console.error('Error executing SQL query:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
+// //Säger att den ska till kursen som har x id och x col (och det blir en url utifrån req.params? )
+// app.get('/courses/:id/:col', async (req, res) => {
+//   // let sql = `SELECT ${req.params.col} FROM courses WHERE id = ${req.params.id}`;
+//   let sql = `SELECT ${req.params.col} FROM courses WHERE id = 1`;
+//   const dbData = await db.query(sql);
+//   res.json(dbData);
 // });
-
-
-// //info from fname, lname and town
-app.get('/moreinfo', async (req, res) => {
-  const { id, town, fname, lname } = req.query;
-
-  let sql = '';
-  let queryParams = [];
-  let paramName = '';
-
-  if (id) {
-    sql = `SELECT * FROM students WHERE id = ${id}`;
-    queryParams.push(id);
-  } else if (town) {
-    sql = `SELECT * FROM students WHERE town = '${town}'`;
-    queryParams.push(town);
-    paramName = 'bor i ' + town;
-  } else if (fname) {
-    sql = `SELECT * FROM students WHERE fname = '${fname}'`;
-    queryParams.push(fname);
-    paramName = 'heter ' + fname + ' i förnamn';
-  } else if (lname) {
-    sql = `SELECT * FROM students WHERE lname = '${lname}'`;
-    queryParams.push(lname);
-    paramName = 'heter ' + lname + ' i efternamn';
-  } else {
-    return res.status(400).send('A valid query parameter (id, town, fname, lname) is required');
-  }
-
-  try {
-    const dbData = await db.query(sql, queryParams);
-
-    if (dbData.length === 0) {
-      return res.status(404).send('No matching records found');
-    }
-
-    const pageTitle = 'More Information on parameter';
-    console.log('dbData');
-    console.log(dbData);
-    res.render('moreinfo', { pageTitle, dbData, paramName });
-
-  } catch (error) {
-    console.error('Error executing SQL query:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-
-
-
-//Säger att den ska till kursen som har x id och x col (och det blir en url utifrån req.params? )
-app.get('/courses/:id/:col', async (req, res) => {
-  // let sql = `SELECT ${req.params.col} FROM courses WHERE id = ${req.params.id}`;
-  let sql = `SELECT ${req.params.col} FROM courses WHERE id = 1`;
-  const dbData = await db.query(sql);
-  res.json(dbData);
-});
-
-
 
 
 
